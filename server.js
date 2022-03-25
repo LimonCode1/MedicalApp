4dbe96483d45b31b63629e2b5596ba658d9a940e
CARLOS ANDRES SANCHEZ <carloss@jarandes.com>
2022-03-24 23:56:47 -0500

    ajuste de conexion sql

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const mysql = require('mysql');
-const myconn = require('express-myconnection');
 const routes = require('./routes');
 
 
 // Server startup
-const app = express();
-app.set('port', process.env.PORT || 5000);
+// const app = express();
+const PORT = process.env.PORT || 5000;
 
-const dbOptions = { // database credentials in clever cloud 
+const connection = mysql.createConnection({
     host: 'us-cdbr-east-05.cleardb.net',
-    port: 3306,
     user: 'b4735e6b2f2213',
     password: 'fc00f36d',
     database: 'heroku_9389f936ccfb0a4'
-}
+});
 
 // Middlewares
-app.use(myconn(mysql, dbOptions, 'single'))// other strategies are pool and request, for the last param
+connection.connect(error => {
+    if (error) throw error;
+    console.log('Database server running!');
+  })
+
 app.use(express.json())
 
 
@@ -29,7 +31,4 @@ app.get('/', (req, res)=>{
 app.use('/api', routes)
 
 // server running 
-app.listen(5000, () => {
-    console.log('Server running on port http://localhost:'+ app.get('port'));
-})
-
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
